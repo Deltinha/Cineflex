696e14c4e8f345f03ef7860efc04a6794bf23633
Matheus <matheus@respondeai.com.br>
2021-09-08 14:46:53 -0300

    preparando o review

--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import * as S from './NavbarStyled';
-
-export default function Navbar(){
-    return (
-        <S.Navbar>
-            <span>CINEFLEX</span>
-        </S.Navbar>
-    );
-}
--- src/routes/sessao/Screen.js
+++
@@ -1,9 +0,0 @@
-import * as S from './ScreenStyled';
-
-export default function Screen({children:screenText}){
-    return (
-        <S.Screen>
-            <div>{screenText}</div>
-        </S.Screen>
-    );
-}
